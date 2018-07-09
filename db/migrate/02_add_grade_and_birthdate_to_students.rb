a5380341b7ed00a405937e648998cf042a625c6c
Scott Weinstein <sweinstein27@gmail.com>
2018-07-09 13:19:16 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,19 +1,6 @@
-class AddGrade < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    connection = ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY,
-    name STRING,
-    genre TEXT,
-    age INTEGER,
-    hometown TEXT
-    )
-    SQL
-
-ActiveRecord::Base.connection.execute(sql)
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 end
